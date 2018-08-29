0cf80cf9e583601ac0e13289b80afe6d340cbd0b
khusnutdinov radmir <xtreme95@yandex.ru>
2018-08-29 18:56:44 +0300

    add handlers

--- nodejs/assignment1/index.js
+++ nodejs/assignment1/index.js
@@ -1,10 +1,41 @@
 var http = require('http');
+var url = require('url');
 
 
-var server = http.createServer();
+var server = http.createServer((req, res)=>{
+
+	const parsedUrl = url.parse(req.url);
+
+	console.log(JSON.stringify(parsedUrl));
+
+});
 
 
 server.listen(3000, ()=>{
 	console.log('NodeJS server listening port 3000.');
 });
+
+
+
+const handlers = {};
+
+
+handlers.hello = function(data, callback) {
+	
+
+
+}
+
+
+handlers.notFound = function(data, callback){
+
+
+
+}
+
+
+const router = {
+	'hello' : handlers.hello,
+	'notFound' : handlers.notFound
+};
 
