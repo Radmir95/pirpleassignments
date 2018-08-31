f0b43b5d49374e05577e63a63d3385e1593d848a
radmir <husnutdinovradmir@gmail.com>
2018-09-01 00:43:20 +0300

    add trimmed path

--- nodejs/assignment1/index.js
+++ nodejs/assignment1/index.js
@@ -6,30 +6,41 @@ var server = http.createServer((req, res)=>{
 
 	const parsedUrl = url.parse(req.url);
 
-	console.log(JSON.stringify(parsedUrl));
+	const path = parsedUrl.pathname;
+
+	const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+
+	const chosenHandler = typeof(router[trimmedPath]) === 'undefined' ? router['notFound'] : router[trimmedPath];
+
+
+	chosenHandler((statusCode)=>{
+		res.end(statusCode.toString());
+
+
+	});
+
 
 });
-
 
 server.listen(3000, ()=>{
 	console.log('NodeJS server listening port 3000.');
 });
 
-
-
 const handlers = {};
 
 
-handlers.hello = function(data, callback) {
-	
+handlers.hello = function(callback) {
+	if (typeof(callback) == 'function')
+		callback(200, {hello: "hello world!"});	
 
 
 }
 
 
-handlers.notFound = function(data, callback){
-
-
+handlers.notFound = function(callback){
+	if (typeof(callback) == 'function')
+		callback(404);
 
 }
 
